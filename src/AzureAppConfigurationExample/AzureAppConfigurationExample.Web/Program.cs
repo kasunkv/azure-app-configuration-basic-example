4bd9bb778335f1ced9c0aa59936f51ae9f4d6c45
Kasun Kodagoda <kvkrusl@gmail.com>
2019-09-28 22:58:10 +0530

    Added Azure App Configuration

--- src/AzureAppConfigurationExample/AzureAppConfigurationExample.Web/Program.cs
+++ src/AzureAppConfigurationExample/AzureAppConfigurationExample.Web/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Logging;
 
 namespace AzureAppConfigurationExample.Web
@@ -18,7 +19,13 @@ namespace AzureAppConfigurationExample.Web
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost
+                .CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(settings["ConnectionStrings:AppConfiguration"]);
+                })
                 .UseStartup<Startup>();
     }
 }
