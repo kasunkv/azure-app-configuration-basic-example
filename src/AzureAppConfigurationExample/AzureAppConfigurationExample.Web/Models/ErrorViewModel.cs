09492f7646829df2bbf55bec9e78901befb53ab5
Kasun Kodagoda <kvkrusl@gmail.com>
2019-09-28 22:38:01 +0530

    Initial commit

---
+++ src/AzureAppConfigurationExample/AzureAppConfigurationExample.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAppConfigurationExample.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
